4a5c723ac480b98e792e81f5cbfd3a4494c24ae1
Fernando Lacunza <flacunza@users.noreply.github.com>
2017-04-28 20:09:52 +0200

    Add files via upload

--- examples/js/word-roulette.js
+++ examples/js/word-roulette.js
@@ -2,7 +2,6 @@ var WordRoulette = function (options) {
 	this._options = options || {};
 	this.target = document.getElementById(this._options.target);
 	this._dataSource = this._options.dataSource;
-	this._mode = this._options.mode || WordRoulette.mode.PLAY;
 	this.render();
 };
 
@@ -17,9 +16,20 @@ WordRoulette.classNames = {
 	GUESSING: "wr-guessing"
 };
 
-WordRoulette.mode = {
-	PLAY: 0,
-	EDIT: 1
+var WRBuilder = function (options) {
+	this._options = options || {};
+	this.target = document.getElementById(this._options.target);
+	this._dataSource = this._options.dataSource;
+	this.render();
+};
+
+WRBuilder.classNames = {
+	BUILDER: "wr-builder",
+	TERM: "wr-term",
+	DEFINITION: "wr-def",
+	HINT: "wr-hint",
+	ADD_BUTTON: "wr-add-btn",
+	EXPORT_BUTTON: "wr-export-btn"
 };
 
 (function () {
@@ -45,56 +55,33 @@ WordRoulette.mode = {
 				e.stopPropagation();
 			});
 			self.target.appendChild(self._hintSection);
-			if (self._mode === WordRoulette.mode.EDIT) {
-				var input = document.createElement('input');
-				input.setAttribute("type", "text");
-				self._termSection.appendChild(input);
-				input = document.createElement('input');
-				input.setAttribute("type", "text");
-				self._definitionSection.appendChild(input);
-				input = document.createElement('input');
-				input.setAttribute("type", "text");
-				self._hintSection.appendChild(input);
-			}
 			self.target.addEventListener('click', function () {
 				self.nextStep();
 			});
 		}
 	};
 	
-	var updateSection = function (section, text) {
-		if (this._mode === WordRoulette.mode.EDIT) {
-			var input = section.getElementsByTagName("input")[0];
-			if (input) {
-				input.value = text;
-			}
-		}
-		else {
-			section.innerHTML = text;
-		}
-	}
-	
-	WordRoulette.prototype.getTermSection = function () {
+	WordRoulette.prototype.getTermSection = WRBuilder.prototype.getTermSection = function () {
 		return this._termSection;
 	};
 
-	WordRoulette.prototype.updateTermSection = function (term) {
+	WordRoulette.prototype.updateTermSection = WRBuilder.prototype.updateTermSection = function (term) {
 		updateSection.call(this, this.getTermSection(), term);
 	};
 	
-	WordRoulette.prototype.getDefinitionSection = function () {
+	WordRoulette.prototype.getDefinitionSection = WRBuilder.prototype.getDefinitionSection = function () {
 		return this._definitionSection;
 	};
 	
-	WordRoulette.prototype.updateDefinitionSection = function (definition) {
+	WordRoulette.prototype.updateDefinitionSection = WRBuilder.prototype.updateDefinitionSection = function (definition) {
 		updateSection.call(this, this.getDefinitionSection(), definition);
 	};
 	
-	WordRoulette.prototype.getHintSection = function () {
+	WordRoulette.prototype.getHintSection = WRBuilder.prototype.getHintSection = function () {
 		return this._hintSection;
 	};
 	
-	WordRoulette.prototype.updateHintSection = function (hint) {
+	WordRoulette.prototype.updateHintSection = WRBuilder.prototype.getHintSection = function (hint) {
 		var self = this;
 		updateSection.call(self, self.getHintSection(), hint);
 		self.target.classList.remove(WordRoulette.classNames.HINT_SHOWN);
@@ -178,7 +165,7 @@ WordRoulette.mode = {
 	};
 
 	
-	WordRoulette.prototype.init = function () {
+	WordRoulette.prototype.init = WRBuilder.prototype.init = function () {
 		var self = this;
 		var req = new XMLHttpRequest();
 		req.open('GET', self._dataSource, true);
@@ -211,7 +198,126 @@ WordRoulette.mode = {
 		entry.hint = hint;
 	};
 	
-	WordRoulette.prototype.exportData = function () {
-		return JSON.stringify(self.data);
+	var updateSection = function (section, text) {
+		if (this instanceof WRBuilder) {
+			var input = section.getElementsByTagName("input")[0];
+			if (input) {
+				input.value = text;
+			}
+		}
+		else {
+			section.innerHTML = text;
+		}
+	};
+	
+	var getSectionValue = function (section) {
+		if (this instanceof WRBuilder) {
+			var input = section.getElementsByTagName("input")[0];
+			if (input) {
+				return input.value;
+			}
+		}
+		else {
+			return section.innerHTML;
+		}
+	};
+
+	WRBuilder.prototype.render = function () {
+		var self = this;
+		if (self.target) {
+			self.target.classList.add(WRBuilder.classNames.BUILDER);
+			self._termSection = document.createElement('section');
+			self._termSection.classList.add(WRBuilder.classNames.TERM);
+			self.target.appendChild(self._termSection);
+			self._definitionSection = document.createElement('section');
+			self._definitionSection.classList.add(WRBuilder.classNames.DEFINITION);
+			self.target.appendChild(self._definitionSection);
+			self._hintSection = document.createElement('section');
+			self._hintSection.classList.add(WRBuilder.classNames.HINT);
+			self.target.appendChild(self._hintSection);
+
+			var input = document.createElement('input');
+			input.setAttribute("type", "text");
+			self._termSection.appendChild(input);
+			input = document.createElement('input');
+			input.setAttribute("type", "text");
+			self._definitionSection.appendChild(input);
+			input = document.createElement('input');
+			input.setAttribute("type", "text");
+			self._hintSection.appendChild(input);
+
+			var button = document.createElement('button');
+			button.innerHTML = "Añadir";
+			button.classList.add(WRBuilder.classNames.ADD_BUTTON);
+			self.target.appendChild(button);
+			button.addEventListener('click', function () {
+				self.addEntry({
+					term: getSectionValue.call(self, self._termSection), 
+					def: getSectionValue.call(self, self._definitionSection)
+				});
+				self.clear();
+			});
+			button = document.createElement('button');
+			button.innerHTML = "Exportar";
+			button.classList.add(WRBuilder.classNames.EXPORT_BUTTON);
+			self.target.appendChild(button);
+			button.addEventListener('click', function () {
+				self.exportData();
+			});
+		}
+	};
+	
+	WRBuilder.prototype.getEntry = function () {};
+	
+	WRBuilder.prototype.clear = function () {
+		var self = this;
+		self.updateTermSection('');
+		self.updateDefinitionSection('');
+		self.updateHintSection('');
+	};
+
+	WRBuilder.prototype.addEntry = function (entry) {
+		var self = this;
+		var term = entry.term.toUpperCase();
+		var initial = term.substr(0, 1);
+		var def = entry.def.toLowerCase();
+		for (var i = 0, ii = self.data.length; i < ii; i++) {
+			var letterSection = self.data[i];
+			if (letterSection.letter === initial) {
+				if (!letterSection.entries.filter(function (elm) {
+					return elm.term === term;
+				}).length) {
+					letterSection.entries.push({
+						term: term,
+						def: def
+					});
+					letterSection.entries.sort(function (a, b) {
+						return a.term.localeCompare(b.term);
+					});
+				}
+				break;
+			}
+		}
+	};
+	
+	WRBuilder.prototype.exportData = function () {
+		var self = this;
+		var blob = new Blob([JSON.stringify(self.data)], { type: 'application/jsonl; charset=utf-8' });
+		var fileName = self._dataSource.substr(self._dataSource.lastIndexOf('/') + 1);
+		if (navigator.msSaveBlob) { // IE 10+
+			navigator.msSaveBlob(blob, fileName);
+		} else {
+			var link = document.createElement("a");
+			if (link.download !== undefined) { // feature detection
+				// Browsers that support HTML5 download attribute
+				var url = URL.createObjectURL(blob);
+				link.setAttribute("href", url);
+				link.setAttribute("download", fileName);
+				link.style.visibility = 'hidden';
+				document.body.appendChild(link);
+				link.click();
+				document.body.removeChild(link);
+			}
+		}
 	};
 })();
